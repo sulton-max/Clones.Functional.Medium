2ef8b8678485c9e50b84f3d82981109a6c5c834b
Max <sultonbek.rakhimov@gmail.com>
2023-08-22 12:05:22 +0500

    Initialized solution

--- src/Medium.FrontEnd/tailwind.config.js
+++ src/Medium.FrontEnd/tailwind.config.js
@@ -1,9 +1,10 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
